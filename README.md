07126417e85731d8de33c839fe97d3801ab726b3
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-02-25 12:52:27 +0000

    production

---
+++ README.md
@@ -0,0 +1,10 @@
+# INSTALLATION
+
+## NEXT JS FRAMEWORK
+npx create-next-app@latest .
+
+## SHADCN
+npx shadcn@latest init
+
+### SHADCN COMPONENTS USED
+input button scroll-area sheet tabs textarea tooltip
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,119 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "+233 54 178 3746",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "adamsmichael46@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "321, Greater Wa, Accra, Ghana",
+  },
+];
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:w-[54%] order-2 xl:order-none">
+            <form
+              action=""
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+            >
+              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <p className="text-white/60">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Distinctio, saepe labore alias, pariatur dignissimos minus quod
+                id, dolore aspernatur assumenda magni reprehenderit officia in
+                velit totam beatae nostrum! Assumenda, laboriosam.
+              </p>
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="text" placeholder="Fullname" />
+                <Input type="email" placeholder="Email" />
+                <Input type="tel" placeholder="Phone number" />
+              </div>
+              {/* select */}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="est">Web Development</SelectItem>
+                    <SelectItem value="cst">UI/UX Design</SelectItem>
+                    <SelectItem value="mst">Logo Design</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              {/* text area */}
+              <Textarea
+                placeholder="Type your message here"
+                className="h-[200px]"
+              />
 
-export default Contact
+              {/* button */}
+              <Button size="md" className="max-w-40" type="submit">
+                Send Message
+              </Button>
+            </form>
+          </div>
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <p className="text-xl">{item.description}</p>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Contact;
